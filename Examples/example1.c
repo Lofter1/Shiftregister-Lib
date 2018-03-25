a1857140bc4c0ee936b86f092d46901a04f32110
Justin <justin099@web.de>
2018-03-26 00:42:25 +0200

    added test for setPin

--- Examples/example1.c
+++ Examples/example1.c
@@ -1,12 +1,17 @@
 #include <avr/io.h>
 #include <util/delay.h>
-#include "../Source/SN74HC595.h"
 
-#define SHIFTREGISTER_DP PB0
-#define SHIFTREGISTER_SHCP PB2
-#define SHIFTREGISTER_STCP PB1
+#define SHIFTREGISTER_DDR DDRB
+#define SHIFTREGISTER_PORT PORTB
+#define SERIALDATA_PIN PB0
+#define SHIFTCLOCK_PIN PB1
+#define STORAGECLOCK_PIN PB2
+
+#include "../Source/shift.h"
 
 #define DELAY 1000
+#define HIGH 1
+#define LOW  0
 
 int main(){
   //Setup
@@ -14,18 +19,37 @@ int main(){
   uint8_t scheme2[SHIFTREGISTER_LENGTH] = {0,1,0,1,0,1,0,1};
   uint8_t byteSchme1 = 0b01100110;
   uint8_t byteSchme2 = 0b10011001;
+  uint8_t status;
 
   DDRB |= 0xFF;
 
   while(1){
-    array2Shiftregister(scheme1);
+    status = array2Shiftregister(scheme1);
     _delay_ms(DELAY);
-    array2Shiftregister(scheme2);
+    status = array2Shiftregister(scheme2);
     _delay_ms(DELAY);
-    byte2Shiftregister(byteSchme1);
+    status = byte2Shiftregister(byteSchme1);
     _delay_ms(DELAY);
-    byte2Shiftregister(byteSchme2);
+    status = byte2Shiftregister(byteSchme2);
     _delay_ms(DELAY);
+
+    status = setPin(D0, LOW);
+    _delay_ms(DELAY);
+    status = setPin(D1, HIGH);
+    _delay_ms(DELAY);
+    status = setPin(D2, HIGH);
+    _delay_ms(DELAY);
+    status = setPin(D3, LOW);
+    _delay_ms(DELAY);
+    status = setPin(D4, LOW);
+    _delay_ms(DELAY);
+    status = setPin(D5, HIGH);
+    _delay_ms(DELAY);
+    status = setPin(D6, HIGH);
+    _delay_ms(DELAY);
+    status = setPin(D7, LOW);
+    _delay_ms(DELAY);
+
   }
 
 }
